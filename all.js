f3f9aab62381b715a284a449abb8733bbd3ea391
鄭舜仁 <randomno@hotmail.com>
2025-09-29 22:31:12 +0800

    fix the img route

--- all.js
+++ all.js
@@ -53,36 +53,36 @@ function render(){
     stationName.textContent = targetData[0].StationName;
     showData.innerHTML =
             `<li class="col-6 card weather text-center">
-                <img src="/img/sun.svg" class="weather-icon px-5 pt-3 card-img-top" alt="天氣示意圖">
+                <img src="./img/sun.svg" class="weather-icon px-5 pt-3 card-img-top" alt="天氣示意圖">
                 <div class="card-body display-4">
                     ${targetData[0].WeatherElement.Weather}
                 </div>
             </li>
             <li class="col-6 card temperature text-center">
-                <img src="/img/temperature.png" class="temperature-icon px-5 pt-3 card-img-top" alt="溫度計圖示">
+                <img src="./img/temperature.png" class="temperature-icon px-5 pt-3 card-img-top" alt="溫度計圖示">
                 <div class="card-body display-4">
                     ${targetData[0].WeatherElement.AirTemperature}℃
                 </div>
             </li>
             <li class="col-6 card humidity text-center">
-                <img src="/img/humidity.png" class="humidity-icon px-5 pt-3 card-img-top" alt="濕度圖示">
+                <img src="./img/humidity.png" class="humidity-icon px-5 pt-3 card-img-top" alt="濕度圖示">
                 <div class="card-body display-4">
                     ${targetData[0].WeatherElement.RelativeHumidity}%
                 </div>
             </li>
             <li class="col-6 card windy  text-center">
-                <img src="/img/windy.png" class="windy-icon px-5 pt-3 card-img-top" alt="風速圖示">
+                <img src="./img/windy.png" class="windy-icon px-5 pt-3 card-img-top" alt="風速圖示">
                 <div class="card-body display-4">
                     ${targetData[0].WeatherElement.WindSpeed}km/hr
                 </div>
             </li>`;
     const weather = getElement('.weather-icon');
-    if (targetData[0].WeatherElement.Weather === "晴") weather.setAttribute("src","/img/sun.svg");
-    else if (targetData[0].WeatherElement.Weather === "多雲") weather.setAttribute("src","/img/cloudy.svg");
-    else if (targetData[0].WeatherElement.Weather === "多雲有雷" || targetData[0].WeatherElement.Weather === "陰有雷") weather.setAttribute("src","/img/cloudyThunder.svg");
-    else if (targetData[0].WeatherElement.Weather === "陰") weather.setAttribute("src","/img/shadow.svg");
-    else if (targetData[0].WeatherElement.Weather === "多雲有雨") weather.setAttribute("src","/img/cloudyRain.svg");
-    else if (targetData[0].WeatherElement.Weather === "晴有靄") weather.setAttribute("src","/img/sunMist.svg");
+    if (targetData[0].WeatherElement.Weather === "晴") weather.setAttribute("src","./img/sun.svg");
+    else if (targetData[0].WeatherElement.Weather === "多雲") weather.setAttribute("src","./img/cloudy.svg");
+    else if (targetData[0].WeatherElement.Weather === "多雲有雷" || targetData[0].WeatherElement.Weather === "陰有雷") weather.setAttribute("src","./img/cloudyThunder.svg");
+    else if (targetData[0].WeatherElement.Weather === "陰") weather.setAttribute("src","./img/shadow.svg");
+    else if (targetData[0].WeatherElement.Weather === "多雲有雨") weather.setAttribute("src","./img/cloudyRain.svg");
+    else if (targetData[0].WeatherElement.Weather === "晴有靄") weather.setAttribute("src","./img/sunMist.svg");
 }
 // 送出測站名，取出資料庫中的資料
 getWeather.addEventListener("click", e => {
